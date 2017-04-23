6610ea6a64e039f467482f7f337043f26003a086
Betty Chempananical <bettymariam@gmail.com>
2017-04-22 23:50:17 -0700

    Created route files - volunteers.js, nonprofits.js. Created volunteers and nonprofits folder in views. Added template files login.ejs, profile.ejs, edit.ejs, search.ejs and dashboard.ejs to non profits folder. Added template files edit.ejs, profile.ejs and dashboard.ejs. Added favicon.ico, images folder and js folder to Public

--- routes/volunteers.js
+++ routes/volunteers.js
@@ -1,9 +1,11 @@
+'use strict'
 const express = require('express');
+const knex = require('../db');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title : 'Welcome'});
+  res.render('index');
 });
 
 module.exports = router;
